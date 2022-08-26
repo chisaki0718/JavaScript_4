374aee3585cbe169802f9fee484c20d4859c3002
chisaki0718 <jobtech.nagamine@gmail.com>
2022-08-26 09:09:30 +0900

    JavaScript課題④修正いたしました。

--- main.js
+++ main.js
@@ -1,90 +1,111 @@
-const quizTitle = document.getElementById('title');
-const quizGenre = document.getElementById('genre');
-const quizDifficulty = document.getElementById('difficulty');
-const startButton = document.getElementById('start-button');
-const quizQuestion = document.getElementById('question');
-const quizAnswers = document.getElementById('answers')
-const backHome = document.getElementById('back-home');
-const backHomeButton = document.createElement('button');
-const apiData = 'https://opentdb.com/api.php?amount=11';
+'use strict'
+//classを用いてプロパティとメソッドなどをまとめる
+class Quiz {
+  constructor (quizData){
+    this._quizzes = quizData.results
+    this._correctAnswersNum = 0
+  }
 
+  getQuizCategory(quizIndex){
+    return this._quizzes[quizIndex -1].category
+  }
 
-let quizIndexNum = 0;
-let correctAnswer = 0;
+  getQuizDifficulty(quizIndex){
+    return this._quizzes[quizIndex -1].difficulty
+  }
 
-/*
-②クイズのデータをjson形式で取得し、さらには配列を取り込む
-非同期処理を実行させつつ、取得できるまでは取得中と表示させる
-*/
-const fetchQuizData = () => {
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(fetch(apiData)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            return Promise.reject(new Error('エラーです'));
-          }
-        })
-        .then(jsonData => {
-          const quizDataList = jsonData.results
-          //③クイズを出力する関数に配列データを渡す
-          createQuiz(quizDataList, quizIndexNum);
-        })
-        .catch(error => {
-          alert('データの取得に失敗しました。')
-        })
-      )
-    }, 1000);
-    quizTitle.innerHTML = `取得中`;
-    quizQuestion.innerHTML = `少々お待ちください`
-    //スタートボタンにactiveが付いていなければ付与しdisplay:none;にする
-    startButton.classList.toggle('active')
-    //もう一度チャレンジする際はカウントを0から始める為にリセットする
-    correctAnswer = 0;
-  });
+  getQuizQuestion(quizIndex){
+    return this._quizzes[quizIndex -1].question
+  }
+
+  getQuizQuestion(quizIndex){
+    return this._quizzes[quizIndex -1].question
+  }
+
+  getCorrectAnswer(quizIndex){
+    return this._quizzes[quizIndex -1].correct_answer
+  }
+
+  getIncorrectAnswers(quizIndex){
+    return this._quizzes[quizIndex -1].incorrect_answers
+  }
+
+  countCorrectAnswers(quizIndex,answer){
+    const correctAnswer = this.getCorrectAnswer(quizIndex)
+    if (correctAnswer === answer) {
+      return this._correctAnswersNum++
+    }
+  }
+
+  getNumQuizzes() {
+    return this._quizzes.length
+  }
+
+  getCorrectAnswersNum() {
+    return this._correctAnswersNum
+  }
 }
-//①startボタンが押されたらクイズのデータを取得する
-startButton.addEventListener('click', fetchQuizData);
 
-//③各クイズを出力する関数
-const createQuiz = (quizDataList, quizIndexNum) => {
-  const quiz = quizDataList[quizIndexNum];
-  quizTitle.innerHTML = `問題${quizIndexNum + 1}`;
-  quizGenre.innerHTML = `【ジャンル】${quiz.category}`;
-  quizDifficulty.innerHTML = `【難易度】${quiz.difficulty}`;
-  quizQuestion.innerHTML = quiz.question;
-  const answers = quiz.incorrect_answers;
-  answers.push(quiz.correct_answer);
+//定数
+const QUIZ_API = 'https://opentdb.com/api.php?amount=10'
+const quizTitle = document.getElementById('title')
+const quizQuestion = document.getElementById('question')
+const quizGenre = document.getElementById('genre')
+const quizDifficulty = document.getElementById('difficulty')
+const startButton = document.getElementById('start-button')
+const answersArea = document.getElementById('answers')
+
+//開始ボタンが押されるとボタンは非表示、データを取得する関数を実行する
+startButton.addEventListener('click',()=>{
+  startButton.hidden = true
+  fetchQuizData(1)
+})
+
+//fetchを使用し、クイズのデータを取得する
+const fetchQuizData = async (index) =>{
+  quizTitle.innerText = '取得中'
+  quizQuestion.innerText = '少々お待ちください'
+  try {
+    const res = await fetch(QUIZ_API)
+    const quizData = await res.json()
+    const quiz = new Quiz(quizData)
+    setNextQuiz(quiz,index) 
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const setNextQuiz = (quiz,index) => {
+  //答えをリセットする
+  answersArea.innerText = ''
+  //配列番号が配列の要素の数(10)以下の間は次のクイズを生成
+  index <= quiz.getNumQuizzes() ? createQuiz(quiz,index) : finishQuiz(quiz)
+}
+
+//クイズを生成する
+const createQuiz = (quiz, quizIndex) => {
+  quizTitle.innerText = `問題${quizIndex}`
+  quizGenre.innerText = `【ジャンル】${quiz.getQuizCategory(quizIndex)}`
+  quizDifficulty.innerText = `【難易度】${quiz.getQuizDifficulty(quizIndex)}`
+  quizQuestion.innerText = quiz.getQuizQuestion(quizIndex)
+
+  const answers = quiz.getIncorrectAnswers(quizIndex)
+  answers.push(quiz.getCorrectAnswer(quizIndex))
   //選択肢(答え)をランダムに出力させる関数
   shuffleQuizAnswers(answers);
 
   answers.forEach(answer => {
-    const createLiAnswers = document.createElement('li');
-    quizAnswers.appendChild(createLiAnswers);
+    const createAnswers = document.createElement('li');
+    answersArea.appendChild(createAnswers);
 
     const createButtonAnswers = document.createElement('button');
-    createButtonAnswers.innerHTML = answer;
-    createLiAnswers.appendChild(createButtonAnswers);
+    createButtonAnswers.innerText = answer;
+    answersArea.appendChild(createButtonAnswers)
 
     createButtonAnswers.addEventListener('click', () => {
-      quizIndexNum++
-      if(createButtonAnswers.innerHTML === quiz.correct_answer){
-        correctAnswer++;
-      }
-      quizAnswers.innerHTML = '';
-      createQuiz(quizDataList, quizIndexNum)
-
-      if (quizIndexNum === 10) {
-        quizTitle.innerHTML = `あなたの正解数は${correctAnswer}です!!`;
-        quizGenre.innerHTML = '';
-        quizDifficulty.innerHTML = '';
-        quizQuestion.innerHTML = `再度チャレンジしたい方は以下をクリック`;
-        quizAnswers.innerHTML = '';
-        backHomeButton.innerHTML = `ホームに戻る`;
-        backHome.appendChild(backHomeButton);
-      }
+      quiz.countCorrectAnswers(quizIndex, answer)
+      quizIndex++
+      setNextQuiz(quiz,quizIndex)
     })
   })
 }
@@ -92,21 +113,23 @@ const createQuiz = (quizDataList, quizIndexNum) => {
 //答えをランダムに出力させる関数
 const shuffleQuizAnswers = answers => {
   for (let i = answers.length - 1; i >= 0; i--) {
-    let rand = Math.floor(Math.random() * (i + 1));
-    [answers[i], answers[rand]] = [answers[rand], answers[i]]
+    let rand = Math.floor(Math.random() * (i + 1))
+    ;[answers[i], answers[rand]] = [answers[rand], answers[i]]
   }
-  return answers;
+  return answers
 }
 
+//クイズ終了時の関数
+const finishQuiz = quiz => {
+  quizTitle.innerText = `あなたの正解数は${quiz.getCorrectAnswersNum()}です!!`
+  quizGenre.innerText = ''
+  quizDifficulty.innerText = ''
+  quizQuestion.innerText = `再度チャレンジしたい方は以下をクリック`
 
-//再チャレンジ用関数
-const restartQuiz = () => {
-  quizTitle.innerHTML = `ようこそ`;
-  quizGenre.innerHTML = '';
-  quizDifficulty.innerHTML = '';
-  quizQuestion.innerHTML = `以下のボタンをクリック`;
-  backHome.innerHTML = '';
-  startButton.classList.toggle('active')
+  const restartButton = document.createElement('button')
+  restartButton.innerText = `ホームに戻る`
+  answersArea.appendChild(restartButton)
+  restartButton.addEventListener('click',() =>{
+    location.reload()
+  })
 }
-//ホームに戻るボタンを押したらホーム（最初の状態）に戻す関数
-backHomeButton.addEventListener('click', restartQuiz);
